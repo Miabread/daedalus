dd6d9abd5c782abfcda26fcec93dc3438b907a3b
jamesBeeProg <jamesbeeprog@gmail.com>
2023-02-21 21:13:38 -0500

    Get tailwind working I think

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import type { Component } from 'solid-js';
 
 const App: Component = () => {
     return (
-        <p class="text-4xl text-green-700 text-center py-20">
-            Hello windi-css!
-        </p>
+        <div class="flex bg-slate-800 h-screen w-screen text-white">
+            <nav class="bg-slate-900 w-1/12">
+                <div class="bg-slate-800 w-full aspect-square">100</div>
+            </nav>
+            <nav></nav>
+            <main></main>
+        </div>
     );
 };
 
