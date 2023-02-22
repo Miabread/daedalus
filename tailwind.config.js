f821b34c39b24136234fe3733de2b67044ed0fc4
jamesBeeProg <jamesbeeprog@gmail.com>
2023-02-21 20:01:20 -0500

    Formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
